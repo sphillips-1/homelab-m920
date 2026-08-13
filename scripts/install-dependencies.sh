d4718c091d09491d08b6403d79392061b7164b8f
Sean <loldrg87@gmail.com>
2026-08-13 15:59:31 -0400

    fixed install dependencies

--- scripts/install-dependencies.sh
+++ scripts/install-dependencies.sh
@@ -43,8 +43,8 @@ fi
 
 if [[ ! -f /etc/apt/sources.list.d/docker.list ]]; then
     cat > /etc/apt/sources.list.d/docker.list <<EOF
-    deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/debian $(. /etc/os-release && echo "$VERSION_CODENAME") stable
-    EOF
+deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/debian $(. /etc/os-release && echo "$VERSION_CODENAME") stable
+EOF
 fi
 
 apt-get update
